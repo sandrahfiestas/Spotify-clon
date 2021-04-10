e5919e6112e7e3a6606f393c77c2c0941662affd
sandrahfiestas <shfiestas@gmail.com>
2021-04-10 02:25:54 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/Styles.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import YourLibrary from './pages/YourLibrary';
+import Premium from './pages/Premium';
+import SongHistory from './pages/SongHistory';
+import Configuration from './pages/Configuration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Search">
+          <Search />
+        </Route>
+        <Route path="/YourLibrary">
+          <YourLibrary />
+        </Route>
+        <Route path="/Premium">
+          <Premium />
+        </Route>
+        <Route path="/SongHistory">
+          <SongHistory />
+        </Route>
+        <Route path="/Configuration">
+          <Configuration />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
