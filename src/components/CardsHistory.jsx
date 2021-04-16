78db7bb2d67a396cd553e7a005c42d875e57951b
sandrahfiestas <shfiestas@gmail.com>
2021-04-16 12:05:24 -0500

    show API images to cards

--- src/components/CardsHistory.jsx
+++ src/components/CardsHistory.jsx
@@ -1,38 +1,23 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import imgHistory1 from '../images/history1.jpg';
-import imgHistory2 from '../images/history2.jpg';
-import imgHistory3 from '../images/history3.jpg';
-import imgHistory4 from '../images/history4.jpg';
-import imgHistory5 from '../images/history5.jpg';
-import imgHistory6 from '../images/history6.jpg';
+import { useDataLayerValue } from '../DataLayer';
 
 function CardsHistory() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+
   return (
     <section className="container-cardsHistory">
-      <div className="container-cardsHistory__card">
-        <img src={imgHistory1} className="container-cardsHistory__card-img" alt="imagen1" />
-        <p className="container-cardsHistory__card-p">Animes</p>
-      </div>
-      <div className="container-cardsHistory__card">
-        <img src={imgHistory2} className="container-cardsHistory__card-img" alt="imagen2" />
-        <p className="container-cardsHistory__card-p">Deep House Top 100</p>
-      </div>
-      <div className="container-cardsHistory__card">
-        <img src={imgHistory3} className="container-cardsHistory__card-img" alt="imagen3" />
-        <p className="container-cardsHistory__card-p">La Reina</p>
-      </div>
-      <div className="container-cardsHistory__card">
-        <img src={imgHistory4} className="container-cardsHistory__card-img" alt="imagen4" />
-        <p className="container-cardsHistory__card-p">Deep House</p>
-      </div>
-      <div className="container-cardsHistory__card">
-        <img src={imgHistory5} className="container-cardsHistory__card-img" alt="imagen5" />
-        <p className="container-cardsHistory__card-p">Deep house</p>
-      </div>
-      <div className="container-cardsHistory__card">
-        <img src={imgHistory6} className="container-cardsHistory__card-img" alt="imagen6" />
-        <p className="container-cardsHistory__card-p">Los de antes...</p>
-      </div>
+      {playlists?.items?.map((playlist) => (
+        <div className="container-cardsHistory__card" key={playlist.id}>
+          <img
+            src={playlist.images[0].url}
+            className="container-cardsHistory__card-img"
+            alt={playlist.name}
+          />
+          <p className="container-cardsHistory__card-p">{playlist.name}</p>
+        </div>
+      ))}
+
     </section>
   );
 }
