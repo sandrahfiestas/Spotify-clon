059621e23b3ea5a3955bc20fed3daa0d3e5cbcaa
sandrahfiestas <shfiestas@gmail.com>
2021-04-18 22:32:41 -0500

    breakpoint styles for desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import Time from './Time';
 import { ReactComponent as HistoryIcon } from '../images/icons/history.svg';
 import { ReactComponent as SettingsIcon } from '../images/icons/settings.svg';
+import { ReactComponent as PreviousBtnIcon } from '../images/icons/btn-left.svg';
+import { ReactComponent as NextBtnIcon } from '../images/icons/btn-right.svg';
+import { useDataLayerValue } from '../DataLayer';
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+
   return (
     <header className="header">
       <div className="header__left">
-        <h2 className="header__left-title">Buenas tardes</h2>
+        <Time />
       </div>
       <div className="header__right">
         <HistoryIcon className="header__right-icon" />
         <SettingsIcon className="header__right-icon" />
+      </div>
+
+      <div className="header__queries header__queries--disabled">
+        <div className="header__queries-btns">
+          <PreviousBtnIcon className="header__queries-btns-Previous" />
+          <NextBtnIcon className="header__queries-btns-Next" />
+        </div>
+        <div className="header__queries-user">
+          <img
+            src={user?.images[0]?.url}
+            alt="Profile"
+            className="header__queries-user-photo"
+          />
+          <p className="header__queries-user-name">{user?.display_name}</p>
+        </div>
       </div>
     </header>
   );
