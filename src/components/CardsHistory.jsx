7778e851090b2fc8da1125a35fd24a16ae15b3c4
sandrahfiestas <shfiestas@gmail.com>
2021-04-22 18:24:54 -0500

    import time component

--- src/components/CardsHistory.jsx
+++ src/components/CardsHistory.jsx
@@ -1,23 +1,30 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useDataLayerValue } from '../DataLayer';
+import Time from './Time';
+import { ReactComponent as PlayIcon } from '../images/icons/play.svg';
 
 function CardsHistory() {
   const [{ playlists }, dispatch] = useDataLayerValue();
 
   return (
     <section className="container-cardsHistory">
-      {playlists?.items?.map((playlist) => (
-        <div className="container-cardsHistory__card" key={playlist.id}>
-          <img
-            src={playlist.images[0].url}
-            className="container-cardsHistory__card-img"
-            alt={playlist.name}
-          />
-          <p className="container-cardsHistory__card-p">{playlist.name}</p>
-        </div>
-      ))}
-
+      <div className="container-cardsHistory__time container-cardsHistory__time--disabled">
+        <Time />
+      </div>
+      <div className="container-cardsHistory__cards">
+        {playlists?.items?.map((playlist) => (
+          <div className="container-cardsHistory__card" key={playlist.id}>
+            <img
+              src={playlist.images[0].url}
+              className="container-cardsHistory__card-img"
+              alt={playlist.name}
+            />
+            <p className="container-cardsHistory__card-p">{playlist.name}</p>
+            <PlayIcon className="container-cardsHistory__card-icon container-cardsHistory__card-icon--disabled" />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
