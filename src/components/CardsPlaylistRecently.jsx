bc1e7f932a309cdc251c73a3a16b1e790c77bec4
sandrahfiestas <shfiestas@gmail.com>
2021-04-25 13:17:16 -0500

    show recently played songs

---
+++ src/components/CardsPlaylistRecently.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useDataLayerValue } from '../DataLayer';
+import { ReactComponent as PlayIcon } from '../images/icons/play.svg';
+
+function CardsPlaylist() {
+  const [{ recentlyPlayedTracks }, dispatch] = useDataLayerValue();
+  console.log('Recientemente 😃', recentlyPlayedTracks);
+
+  // const [{ topArtists }, dispatch] = useDataLayerValue();
+  // console.log('Artistas Top 🤯👉 ', topArtists);
+
+  // const [{ topTracks }, dispatch] = useDataLayerValue();
+  // console.log('Pistas principales 🎶', topTracks);
+
+  // const [{ categories }, dispatch] = useDataLayerValue();
+  // console.log('Categorias 💪', categories);
+
+  return (
+    <section className="container-cardsPlaylist">
+      <div className="container-cardsPlaylist__title">
+        <p>Escuchado recientemente</p>
+      </div>
+      <div className="container-cardsPlaylist__cards">
+
+        {recentlyPlayedTracks?.items?.map((recentlyPlayedTrack) => (
+          <div
+            className="container-cardsPlaylist__card"
+            key={recentlyPlayedTrack.track.id}
+          >
+            <img
+              src={recentlyPlayedTrack.track.album.images[0].url}
+              className="container-cardsPlaylist__card-img"
+              alt={recentlyPlayedTrack.track.name}
+            />
+            <PlayIcon className="container-cardsPlaylist__card-icon container-cardsPlaylist__card-icon--disabled" />
+            <p className="container-cardsPlaylist__card-p">
+              {recentlyPlayedTrack.track.name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default CardsPlaylist;
