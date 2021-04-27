f9902f501fc369ee50ef3612dd26d5928da21c93
sandrahfiestas <shfiestas@gmail.com>
2021-04-26 22:38:50 -0500

    show top artists

---
+++ src/components/CardsPlaylists.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDataLayerValue } from '../DataLayer';
+import { ReactComponent as PlayIcon } from '../images/icons/play.svg';
+
+function CardsPlaylist() {
+  const [{ topArtists }, dispatch] = useDataLayerValue();
+  console.log('Artistas Top 🤯👉 ', topArtists);
+
+  // const [{ topTracks }, dispatch] = useDataLayerValue();
+  // console.log('Pistas principales 🎶', topTracks);
+
+  // const [{ categories }, dispatch] = useDataLayerValue();
+  // console.log('Categorias 💪', categories);
+
+  return (
+    <section className="container-cardsPlaylist">
+      <div className="container-cardsPlaylist__title">
+        <Link to="/allCards" className="container-cardsPlaylist__title-p">
+          Tus Mejores Artistas
+        </Link>
+        <Link to="/allCards" className="container-cardsPlaylist__title-all">
+          VER TODO
+        </Link>
+      </div>
+      <div className="container-cardsPlaylist__cards">
+        {topArtists?.items?.map((topArtist) => (
+          <div className="container-cardsPlaylist__card" key={topArtist.id}>
+            <img
+              src={topArtist.images[0].url}
+              className="container-cardsPlaylist__card-img"
+              alt={topArtist.name}
+            />
+            <PlayIcon className="container-cardsPlaylist__card-icon container-cardsPlaylist__card-icon--disabled" />
+            <p className="container-cardsPlaylist__card-p">{topArtist.name}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default CardsPlaylist;
