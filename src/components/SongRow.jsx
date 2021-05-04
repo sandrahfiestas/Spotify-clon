6a4eb07d7b01812e2237f43e9263bd073ed767c4
sandrahfiestas <shfiestas@gmail.com>
2021-05-03 23:51:08 -0500

    show song row

---
+++ src/components/SongRow.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+function SongRow({ track }) {
+  console.log('Track 👀', track);
+  return (
+    <div className="songRow">
+      <img className="songRow__album" src={track.album.images[0].url} alt="" />
+      <div className="songRow__description">
+        <h1 className="songRow__description-title">{track.name}</h1>
+        <p className="songRow__description-p">
+          {track.artists.map((artist) => artist.name).join(', ')}
+          -
+          {' '}
+          {track.album.name}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SongRow;
